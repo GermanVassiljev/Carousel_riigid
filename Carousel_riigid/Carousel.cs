cadb6e82558becdeda9fac6ffeb9a7a65ee497e0
opilane <opilane@HP-CZC2349HTZ>
2023-04-14 14:44:55 +0300

    Add project files.

---
+++ Carousel_riigid/Carousel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Carousel_riigid
+{
+    public class Carousel : CarouselPage
+    {
+        public Carousel()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                var Page = new ContentPage
+                {
+                    TabIndex = i,                   
+                    Content = new StackLayout
+                    {
+                        Children =
+                        {
+                            new BoxView
+                            {
+
+                            },
+                            
+                            new Button
+                            {
+                                BackgroundColor= Color.DarkBlue,
+                                TextColor= Color.White,
+                            }
+                        }
+                    }
+                };
+                Children.Add(Page);
+            }
+            
+            
+        }
+    }
+}
