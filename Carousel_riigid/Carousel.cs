1d395cf2c4e88453fcaf4bba034d4d452e31ff6b
opilane <opilane@HP-CZC2349HTZ>
2023-04-19 10:31:44 +0300

    1

--- Carousel_riigid/Carousel.cs
+++ Carousel_riigid/Carousel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.IO;
 using Xamarin.Forms;
 
 namespace Carousel_riigid
@@ -11,7 +11,30 @@ namespace Carousel_riigid
     {
         public Carousel()
         {
-            for (int i = 1; i <= 5; i++)
+            string filename = "Riigid_pealinnad.txt";
+            string text = "Moskva;Venemaa\n" + "London;Suurbritannia\n" + "Stockholm;Rootsi";
+
+            // Get the path to the Documents folder for the app
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), filename);
+
+            // Combine the path with the filename to create the full file path
+            string filePath = Path.Combine(path, filename);
+
+            // Write the text to the file
+            File.WriteAllText(path, text);
+            var pealinnad = new Dictionary<int, string>()
+            {
+                {1,"Moskva"},
+                { 2,"Stockholm"},
+                { 3, "London" }
+            };
+            var riigid = new Dictionary<int, string>()
+            {
+                {1, "Venemaa" },
+                { 2, "Rootsi" },
+                {3, "Suurbritannia" }
+            };
+            for (int i = 1; i <= 3; i++)
             {
                 var Page = new ContentPage
                 {
@@ -20,21 +43,27 @@ namespace Carousel_riigid
                     {
                         Children =
                         {
-                            new BoxView
+                            new Label
                             {
-
+                                FontSize= 20,
+                                Text = File.ReadAllText(path)
                             },
-                            
+                            new Editor
+                            {
+                                Placeholder = "Riig"
+                            },
                             new Button
                             {
-                                BackgroundColor= Color.DarkBlue,
-                                TextColor= Color.White,
-                            }
+                                BackgroundColor = Color.DarkOrange,                                
+                            }                            
                         }
                     }
                 };
                 Children.Add(Page);
+                var editor = ((StackLayout)Page.Content).Children[1] as Editor;
+                
             }
+
             
             
         }
--- Carousel_riigid/MainPage.xaml.cs
+++ Carousel_riigid/MainPage.xaml.cs
@@ -10,9 +10,23 @@ namespace Carousel_riigid
 {
     public partial class MainPage : ContentPage
     {
+        Button corousel;
         public MainPage()
         {
-            
+            StackLayout stack = new StackLayout(); 
+            corousel= new Button()
+            {
+                Text = "Pealinnad ja need riigid",
+                BackgroundColor= Color.DarkOrange,  
+            };
+            corousel.Clicked += Corousel_Clicked;
+            stack.Children.Add( corousel );
+            Content= stack;
+        }
+
+        private async void Corousel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new Carousel()));
         }
     }
 }
