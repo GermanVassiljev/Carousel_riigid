228445566fd31583c19d9acb5e20da41e93a5f66
opilane <opilane@HP-CZC2349HTZ>
2023-04-21 13:47:42 +0300

    3

--- Carousel_riigid/Carousel.cs
+++ Carousel_riigid/Carousel.cs
@@ -11,36 +11,19 @@ using System.Runtime.InteropServices.ComTypes;
 namespace Carousel_riigid
 {
     public class Carousel : CarouselPage
-    {
-        string text = "Moskva:Venemaa;" + "London:Suurbritannia;" + "Stockholm:Rootsi;";
-        string filename = "Riigid_pealinnad.txt";
-        public Carousel()
-        {
+    {       
+        string[] dir;
+        string[] linnad_riigid;
+        public Carousel(string path)
+        {           
 
-            // Get the path to the Documents folder for the app
-            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), filename);
-
-            // Combine the path with the filename to create the full file path
-            string filePath = Path.Combine(path, filename);
-
-            // Write the text to the file
-            File.WriteAllText(path, text);
-
-            string dir_FilePath = Path.Combine(path, filename);
+            string dir_FilePath = Path.Combine(path, "Riigid_pealinnad.txt");
             string dir_Text = File.ReadAllText(path);
-            string[] dir = dir_Text.Split(';');
+            dir = dir_Text.Split(';');
             int length = dir.Length;
             string direct;
-            string[] linnad_riigid;
-            //for (int i = 0; i < dir.Length; i++)
-            //{
-            //    direct = dir[i];
-            //    //linnad_riigid.SetValue(direct.Split(':'), i);
-            //    linnad_riigid = direct.Split(':');
-            //}            
 
-
-
+            
 
             for (int i = 0; i < length - 1 ; i++)
             {
@@ -48,55 +31,56 @@ namespace Carousel_riigid
                 linnad_riigid = rnd.Split(':');
                 var Page = new ContentPage
                 {
-                    TabIndex = i,
                     Content = new StackLayout
                     {
+                        TabIndex= i,
                         Children =
-                        {
+                        {                            
                             new Label
                             {
                                 FontSize= 20,
-                                Text = linnad_riigid[0]
-                            },
-                            new Button
-                            {
-                                BackgroundColor = Color.DarkOrange,
+                                Text = linnad_riigid[0],
+                                BackgroundColor= Color.DarkOrange,
+                                TabIndex= i
                             }
                         }
                     }
 
                 };
                 Children.Add(Page);
-                //var editor = ((StackLayout)Page.Content).Children[1] as Editor;
+                var lbl = ((StackLayout)Page.Content).Children[0] as Label;
+                var tap = new TapGestureRecognizer();
+                lbl.GestureRecognizers.Add(tap);
+                tap.Tapped += Tap_Tapped;
 
             }
+
 
 
 
 
 
         }
-        private string ReadLine(int arv_leht, string path)
+        bool OnOff = true;
+        private void Tap_Tapped(object sender, EventArgs e)
         {
-            using (StreamReader reader = new StreamReader(path))
+            if (OnOff)
             {
-                for (int i = 1; i <= arv_leht; i++)
-                {
-                    if (reader.EndOfStream)
-                    {
-                        break;
-                    }
-                    if (i == arv_leht)
-                    {
-                        return reader.ReadLine();
-                    }
-                    else
-                    {
-                        reader.ReadLine(); // skip this line and continue reading
-                    }
-                }
-                return "";
+                var lbl = sender as Label;
+                string Tekst_on = dir[lbl.TabIndex];
+                linnad_riigid = Tekst_on.Split(':');
+                lbl.Text += " - " + linnad_riigid[1];
+                OnOff= false;
             }
+            else
+            {
+                var lbl = sender as Label;
+                string Tekst_on = dir[lbl.TabIndex];
+                linnad_riigid = Tekst_on.Split(':');
+                lbl.Text = linnad_riigid[0];
+                OnOff= true;
+            }
+            
         }
     }
 }
--- Carousel_riigid/MainPage.xaml.cs
+++ Carousel_riigid/MainPage.xaml.cs
@@ -3,30 +3,68 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Carousel_riigid
 {
     public partial class MainPage : ContentPage
     {
-        Button corousel;
+        string text = "Moskva:Venemaa;" + "London:Suurbritannia;" + "Stockholm:Rootsi;";
+        Button corousel, uus_kart;
+        Editor uus_linn, uus_riik;
+        Label uus;
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Riigid_pealinnad.txt");
         public MainPage()
         {
+            File.WriteAllText(path, text);
             StackLayout stack = new StackLayout(); 
             corousel= new Button()
             {
                 Text = "Pealinnad ja need riigid",
                 BackgroundColor= Color.DarkOrange,  
             };
+            uus = new Label()
+            {
+                Text = "Kirjuta uus pealinn ja riik",
+                BackgroundColor= Color.DarkOrange
+            };
+            uus_linn = new Editor()
+            {
+                Placeholder = "Pealinn"
+            };
+            uus_riik = new Editor()
+            {
+                Placeholder = "Riik"
+            };
+            uus_kart = new Button()
+            {
+                Text = "Lisa uus kart",
+                BackgroundColor= Color.DarkOrange
+            };
             corousel.Clicked += Corousel_Clicked;
             stack.Children.Add( corousel );
-            Content= stack;
+            stack.Children.Add(uus);
+            stack.Children.Add(uus_linn);
+            stack.Children.Add(uus_riik);
+            stack.Children.Add(uus_kart);
+            uus_kart.Clicked += Uus_kart_Clicked;
+            Content = stack;
+        }
+        string uus_tekst;
+        private void Uus_kart_Clicked(object sender, EventArgs e)
+        {
+            uus_tekst = uus_linn.Text+":"+uus_riik.Text+";";
+            File.AppendAllText(path, uus_tekst);
+            uus_linn.Text = null;
+            uus_riik.Text = null;
         }
 
         private async void Corousel_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new NavigationPage(new Carousel()));
+            await Navigation.PushAsync(new NavigationPage(new Carousel(path)));
         }
     }
 }
